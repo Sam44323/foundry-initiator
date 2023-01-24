108fa5eda52ec9fed0e92dd505d2f202eecc82e0
Suranjan <suranjanbiswas2002@gmail.com>
2023-01-24 23:46:39 +0530

    add: added custom fuzz-testing options

--- test/StakeContract.t.sol
+++ test/StakeContract.t.sol
@@ -8,16 +8,15 @@ import "../src/StakeContract.sol";
 contract CounterTest is Test {
     StakeContract public stakeContract;
     ERC20 public token;
-    uint256 public constant AMOUNT = 10e18; // 10 tokens
 
     function setUp() public {
         stakeContract = new StakeContract();
         token = new TestToken("Test", "TST");
     }
 
-    function testStake() public {
-        token.approve(address(stakeContract), AMOUNT);
-        bool success = stakeContract.stake(AMOUNT, address(token));
+    function testStake(uint16 amount) public {
+        token.approve(address(stakeContract), amount);
+        bool success = stakeContract.stake(amount, address(token));
         assertTrue(success);
     }
 }
