c4b7d9bea75c04afa35bd98638115ed365b6adff
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-08 02:29:43 +0530

    aadd: added msg.sender prank setter for the contracts

--- test/Auth.t.sol
+++ test/Auth.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.17;
 
 import "forge-std/Test.sol";
 import "../src/Wallet.sol";
+import "forge-std/console.sol";
 
 contract AuthTest is Test {
     Wallet public wallet;
@@ -19,5 +20,26 @@ contract AuthTest is Test {
     function testFailNotOwner() public {
         vm.prank(address(1)); // this changes the address of msg.sender
         wallet.setOwner(address(1));
+    }
+
+    function testFailSetOwnerRepeat() public {
+        wallet.setOwner(address(1));
+
+        vm.startPrank(address(1)); // Set all subsequent msg.sender to address(1)
+
+        // all calls made from address(1)
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+        wallet.setOwner(address(1));
+
+        // Reset all subsequent msg.sender to address(this)
+        vm.stopPrank();
+
+        console.log("owner: ", wallet.owner());
+
+        // call made from address(this) - this will fail
+        wallet.setOwner(address(1));
+
+        console.log("owner", wallet.owner());
     }
 }
