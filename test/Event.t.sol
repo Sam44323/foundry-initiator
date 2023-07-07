c3c76796cf57b80245a01b0fe625301fed4865bd
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-08 03:03:16 +0530

    uupdate: updated the imports

--- test/Event.t.sol
+++ test/Event.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
-import "forge-std/Test.sol";
 import "../src/Event.sol";
+import "forge-std/Test.sol";
 
 contract EventTest is Test {
     Event public eve;
