74b263734760fd4345ae14f4b4340c82e23d3ccd
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-08 02:59:04 +0530

    aadd: added testers for the event

--- src/Event.sol
+++ src/Event.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.18;
+pragma solidity 0.8.17;
 
 contract Event {
     event Transfer(address indexed from, address indexed to, uint256 amount);
@@ -7,11 +7,7 @@ contract Event {
         emit Transfer(from, to, amount);
     }
 
-    function transferMany(
-        address from,
-        address[] calldata to,
-        uint256[] calldata amounts
-    ) external {
+    function transferMany(address from, address[] calldata to, uint256[] calldata amounts) external {
         for (uint256 i = 0; i < to.length; i++) {
             emit Transfer(from, to[i], amounts[i]);
         }
--- test/Event.t.sol
+++ test/Event.t.sol
@@ -7,7 +7,35 @@ import "../src/Event.sol";
 contract EventTest is Test {
     Event public eve;
 
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
     function setUp() public {
         eve = new Event();
+    }
+
+    function testEmitEventTransfer() public {
+        // function expectEmit(
+        //     bool checkTopic1,
+        //     bool checkTopic2,
+        //     bool checkTopic3,
+        //     bool checkData
+        // ) external;
+        // 1. Tell Foundry which data to check
+        // Check index 1, index 2 and data
+
+        vm.expectEmit(true, true, false, true);
+
+        // 2. Emit the expected event
+        emit Transfer(address(this), address(123), 456);
+
+        // 3. Call the function that should emit the event
+        eve.transfer(address(this), address(123), 456);
+
+        // Check only index 1
+        vm.expectEmit(true, false, false, false);
+        emit Transfer(address(this), address(123), 456);
+
+        // NOTE: index 2 and data (amount) doesn't match but the test will still pass
+        eve.transfer(address(this), address(111), 222);
     }
 }
