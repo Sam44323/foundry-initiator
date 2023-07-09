d5d62450c6bfc768d675c23c43baed215370f93f
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-09 14:09:29 +0530

    add: added misc tests for auction data

--- test/Time.t.sol
+++ test/Time.t.sol
@@ -5,9 +5,52 @@ import "forge-std/Test.sol";
 import "../src/Time.sol";
 
 contract AuctionTest is Test {
-    Auction public time;
+    Auction public auction;
+    uint256 public startAt;
+
+    // NOTE
+    // vm.warp - set block.timestamp to future timestamp
+    // vm.roll - set block.number
+    // skip - increment current timestamp
+    // rewind - decrement current timestamp
 
     function setUp() public {
-        time = new Auction();
+        auction = new Auction();
+        startAt = block.timestamp;
+    }
+
+    function testBidFailsBeforeStartTime() public {
+        vm.expectRevert(bytes("cannot bid"));
+        auction.bid();
+    }
+
+    function testBid() public {
+        vm.warp(startAt + 1 days);
+        auction.bid();
+    }
+
+    function testBidFailsAfterEndTime() public {
+        vm.expectRevert(bytes("cannot bid"));
+        vm.warp(startAt + 2 days);
+        auction.bid();
+    }
+
+    //misc
+
+    function testTimestamp() public {
+        uint256 t = block.timestamp;
+        // set block.timestamp to t + 100
+        skip(100);
+        assertEq(block.timestamp, t + 100);
+
+        // set block.timestamp to t + 100 - 100;
+        rewind(100);
+        assertEq(block.timestamp, t);
+    }
+
+    function testBlockNumber() public {
+        // set block number to 11
+        vm.roll(11);
+        assertEq(block.number, 11);
     }
 }
