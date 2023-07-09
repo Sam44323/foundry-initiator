fde6d31af3b05948992806cc602d697b24f361c4
Suranjan <suranjanbiswas2002@gmail.com>
2023-07-09 15:59:54 +0530

    add: added a helper function for sending the eth

--- test/Wallet.t.sol
+++ test/Wallet.t.sol
@@ -8,7 +8,16 @@ import "../src/Wallet.sol";
 contract WalletTest is Test {
     Wallet public wallet;
 
+    // NOTES
+    // deal(address, uint) - Set balance of address
+    // hoax(address, uint) - deal + prank, Sets up a prank and set balance
+
     function setUp() public {
         wallet = new Wallet();
+    }
+
+    function _send(uint amount) private {
+        (bool ok, ) = address(wallet).call{value: amount}("");
+        require(ok, "send failed");
     }
 }
